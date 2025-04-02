4f1359cd5b9550cf25923bcc7d8ae41d0e23f38e
Macking93 <156823807+elmackico93@users.noreply.github.com>
2025-04-02 17:26:51 +0200

    Removed backup and changed favicon.ico

--- backups/components/search/errors-backup/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SearchError } from './SearchError';
-export { default as SearchFallback } from './SearchFallback';
-export { default as SearchErrorBoundary } from './SearchErrorBoundary';
